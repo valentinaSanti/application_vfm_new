de0ae5c611bc10a95fef35320a3555704b1b4e17
Matteo Carraro <matteo.carraro.10@studenti.unipd.it>
2023-04-23 23:48:48 +0200

    Ho provato a sistemare gender selector sulla schermata login Creata nuova pagina app-general_theme, vedremo se implementarla o meno

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:application_vfm_new/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:application_vfm_new/app_general_theme.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-class Login extends StatelessWidget {
+List<String> option = ['MALE','FEMALE'];
+
+
+
+class Login extends StatefulWidget{
   Login({Key? key}) : super(key: key);
 
-  static const route = '/login/';
-  static const routename = 'Login';
+  @override
+  State<Login> createState() => LoginState();
+}
+
+class LoginState extends State<Login> {
+
+  //static const route = '/login/';
+  //static const routename = 'Login';
+  String currentOption = option[0];
 
   final _formKey = GlobalKey<FormState>();
 
   final TextEditingController ageController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -39,69 +52,96 @@ class Login extends StatelessWidget {
                       children: <Widget>[
                         const SizedBox(width: 10),
                         const Text('Gender',
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 218, 162, 21),
-                                fontSize: 17)),
-                        Radio(
-                          fillColor: MaterialStateColor.resolveWith(
-                              (states) => const Color(0xFF89453C)),
-                          value: 1,
-                          groupValue: 1,
-                          onChanged: (val) {},
-                        ),
-                        const Text(
-                          'MALE',
-                          style: TextStyle(fontSize: 17.0),
-                        ),
-                        Radio(
-                            fillColor: MaterialStateColor.resolveWith(
-                                (states) => const Color(0xFF89453C)),
-                            value: 2,
-                            groupValue: 1,
-                            onChanged: (val) {}),
-                        const Text(
-                          'FEMALE',
                           style: TextStyle(
-                            fontSize: 17.0,
-                          ),
+                            color: Color.fromARGB(255, 218, 162, 21),
+                            fontSize: 17
+                          )
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: 400,
-                      child: TextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter your age';
-                            } else if (int.tryParse(value) == null) {
-                              return 'Please enter an integer valid number';
-                            }
-                            return null;
-                          },
-                          controller: ageController,
-                          enabled: true,
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      width: 1, color: Color(0xFF89453C))),
-                              labelText: 'Age',
-                              labelStyle: TextStyle(color: Color(0xFF89453C)))),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        ElevatedButton(
-                            onPressed: () async {
-                              if (_formKey.currentState!.validate()) {}
+                        //Radio(
+                        //  fillColor: MaterialStateColor.resolveWith(
+                        //      (states) => const Color(0xFF89453C)),
+                        //  value: 0,
+                        //  groupValue: 1,
+                        //  onChanged: (val) {},
+                        //),
+                        ListTile(
+                          title: const Text('MALE'),
+                          leading: Radio(
+                            value: option[0],
+                            groupValue: currentOption,
+                            onChanged:(value) {
+                              setState((){
+                                currentOption : value.toString();
+                              });
                             },
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color(0xFF83AA99),
-                                shape: const CircleBorder()),
-                            child: const Icon(Icons.check)),
+                          )
+                        ),
+                        //const Text(
+                        //  'MALE',
+                        //  style: TextStyle(fontSize: 17.0),
+                        //),
+                        //Radio(
+                        //    fillColor: MaterialStateColor.resolveWith(
+                        //        (states) => const Color(0xFF89453C)),
+                        //    value: 2,
+                        //    groupValue: 1,
+                        //    onChanged: (val) {}
+                        //),
+                        ListTile(
+                          title: const Text('FEMALE'),
+                          leading: Radio(
+                            value: option[1],
+                            groupValue: currentOption,
+                            onChanged:(value) {
+                              setState(() {
+                                currentOption : value.toString();
+                              });
+                            },
+                          )
+                        ),
+                        //const Text(
+                        //  'FEMALE',
+                        //  style: TextStyle(
+                        //    fontSize: 17.0,
+                        //  ),
+                        //),
                       ],
                     ),
+//                    const SizedBox(height: 20),
+//                    SizedBox(
+//                      width: 400,
+//                      child: TextFormField(
+//                          validator: (value) {
+//                            if (value == null || value.isEmpty) {
+//                              return 'Please enter your age';
+//                            } else if (int.tryParse(value) == null) {
+//                              return 'Please enter an integer valid number';
+//                            }
+//                            return null;
+//                          },
+//                          controller: ageController,
+//                          enabled: true,
+//                          decoration: const InputDecoration(
+//                              border: OutlineInputBorder(),
+//                              focusedBorder: OutlineInputBorder(
+//                                  borderSide: BorderSide(
+//                                      width: 1, color: Color(0xFF89453C))),
+//                              labelText: 'Age',
+//                              labelStyle: TextStyle(color: Color(0xFF89453C)))),
+//                    ),
+//                    Row(
+//                      mainAxisAlignment: MainAxisAlignment.end,
+//                      children: [
+//                        ElevatedButton(
+//                            onPressed: () async {
+//                              if (_formKey.currentState!.validate()) {}
+//                            },
+//                            style: ElevatedButton.styleFrom(
+//                                backgroundColor: const Color(0xFF83AA99),
+//                                shape: const CircleBorder()),
+//                            child: const Icon(Icons.check)),
+//                      ],
+//                    ),
                   ],
                 ),
               ),
