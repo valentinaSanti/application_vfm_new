f8874310b45c4f5b96cbe6f9abfac09d3a3d656a
Francesca DAmato <damato.francesca98@gmail.com>
2023-04-27 23:03:20 +0200

    home aggiornata

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -24,62 +24,75 @@ class _HomeState extends State<Home> {
   ];
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 197, 233, 152),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            ListTile(
-                leading: const Icon(MdiIcons.logout),
-                title: const Text('Logout'),
-                // delete all data from the database
-                onTap: () => {}),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text('About'),
-            ),
-            ListTile(
-                leading: const Icon(MdiIcons.bookInformationVariant),
-                title: const Text('App Information 1'),
-                onTap: () => {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => InfoApp(),
-                      ))
-                    }),
-            ListTile(
-                leading: const Icon(MdiIcons.bookHeart),
-                title: const Text('Information about using data'),
-                onTap: () => {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => info_dati_usati(),
-                      ))
-                    }),
-          ],
+    return ChangeNotifierProvider<HomeProvider>(
+      create: (context) => HomeProvider(),
+      builder: (context, child) => Scaffold(
+        backgroundColor: Color.fromARGB(255, 197, 233, 152),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              ListTile(
+                  leading: const Icon(MdiIcons.logout),
+                  title: const Text('Logout'),
+                  // delete all data from the database
+                  onTap: () => {}),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('About'),
+              ),
+              ListTile(
+                  leading: const Icon(MdiIcons.bookInformationVariant),
+                  title: const Text('App Information 1'),
+                  onTap: () => {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => InfoApp(),
+                        ))
+                      }),
+              ListTile(
+                  leading: const Icon(MdiIcons.bookHeart),
+                  title: const Text('Information about using data'),
+                  onTap: () => {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => info_dati_usati(),
+                        ))
+                      }),
+            ],
+          ),
         ),
-      ),
-      appBar: AppBar(
-        iconTheme:
-            const IconThemeData(color: Color.fromARGB(255, 218, 162, 21)),
-        elevation: 0,
-        backgroundColor: Color.fromARGB(255, 199, 237, 163),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: IconButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          fullscreenDialog: true,
-                          builder: (context) => Login()));
+        appBar: AppBar(
+          iconTheme:
+              const IconThemeData(color: Color.fromARGB(255, 218, 162, 21)),
+          elevation: 0,
+          backgroundColor: Color.fromARGB(255, 199, 237, 163),
+          actions: [
+            IconButton(
+                padding: const EdgeInsets.only(left: 8.0, top: 8, bottom: 8),
+                onPressed: () async {
+                  Provider.of<HomeProvider>(context, listen: false);
                 },
                 icon: const Icon(
                   MdiIcons.accountCircle,
                   size: 40,
                   color: Color.fromARGB(255, 218, 148, 18),
                 )),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            fullscreenDialog: true,
+                            builder: (context) => Login()));
+                  },
+                  icon: const Icon(
+                    MdiIcons.accountCircle,
+                    size: 40,
+                    color: Color.fromARGB(255, 235, 147, 24),
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
