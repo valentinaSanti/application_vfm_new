4fb7225fef9d4bf5280db9f156765990697dcb40
Francesca DAmato <damato.francesca98@gmail.com>
2023-05-06 12:06:05 +0200

    titolo home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -37,47 +35,72 @@ class _HomeState extends State<Home> {
             children: [
               const Padding(
                 padding: EdgeInsets.all(8.0),
-                
               ),
               ListTile(
-                  leading: const Icon(MdiIcons.bookInformationVariant,
-                  size: 30,
-                  color: Color.fromARGB(255, 215, 137, 27),),
-                  title: const Text('App Information 1',
-                  style: TextStyle(fontSize: 20,fontStyle: FontStyle.normal,color: Color.fromARGB(255, 25, 25, 25)),),
+                  leading: const Icon(
+                    MdiIcons.bookInformationVariant,
+                    size: 30,
+                    color: Color.fromARGB(255, 215, 137, 27),
+                  ),
+                  title: const Text(
+                    'App Information 1',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontStyle: FontStyle.normal,
+                        color: Color.fromARGB(255, 25, 25, 25)),
+                  ),
                   onTap: () => {
                         Navigator.of(context).push(MaterialPageRoute(
                           builder: (context) => InfoApp(),
                         ))
                       }),
               ListTile(
-                  leading: const Icon(MdiIcons.bookHeart,size: 30,
-                  color: Color.fromARGB(255, 215, 137, 27),),
+                  leading: const Icon(
+                    MdiIcons.bookHeart,
+                    size: 30,
+                    color: Color.fromARGB(255, 215, 137, 27),
+                  ),
                   title: const Text('Information about using data',
-                  style: TextStyle(fontSize: 20,)),
+                      style: TextStyle(
+                        fontSize: 20,
+                      )),
                   onTap: () => {
                         Navigator.of(context).push(MaterialPageRoute(
                           builder: (context) => info_dati_usati(),
                         ))
                       }),
-              const Padding(padding: 
-              EdgeInsets.all(280.0)
-              ),
-
+              const Padding(padding: EdgeInsets.all(280.0)),
               ListTile(
-                  leading: const Icon(MdiIcons.logout,
-                  size: 40,),
-                  title: const Text('Logout', 
-                  style: TextStyle(fontSize: 30),
+                  leading: const Icon(
+                    MdiIcons.logout,
+                    size: 40,
+                  ),
+                  title: const Text(
+                    'Logout',
+                    style: TextStyle(fontSize: 30),
                   ),
                   onTap: () => {
-                    _toLoginPage(context),
-                  }),
-              
+                        _toLoginPage(context),
+                      }),
             ],
           ),
         ),
         appBar: AppBar(
+          title: Text(
+            'Application VFM',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              foreground: Paint()
+                ..shader = LinearGradient(
+                  colors: [Colors.orange.shade300, Colors.orange.shade700],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0,
+                    70.0)), // Aggiunge il gradiente come sfondo del testo del titolo
+            ),
+          ),
+          centerTitle: true,
           iconTheme:
               const IconThemeData(color: Color.fromARGB(255, 218, 162, 21)),
           elevation: 0,
@@ -114,16 +137,16 @@ class _HomeState extends State<Home> {
       ),
     );
   }
-   void _toLoginPage(BuildContext context) async{
-    //Unset the 'username' filed in SharedPreference 
+
+  void _toLoginPage(BuildContext context) async {
+    //Unset the 'username' filed in SharedPreference
     final sp = await SharedPreferences.getInstance();
     sp.remove('username');
 
-    //Pop the drawer first 
+    //Pop the drawer first
     Navigator.pop(context);
     //Then pop the HomePage
-    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()));
-  }//_toCalendarPage
-
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()));
+  } //_toCalendarPage
 } //HomePage
-
